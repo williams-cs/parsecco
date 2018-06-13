0db9b32db65b58e4d13d4599c1174aa84f3cc33e
Daniel W. Barowy <dbarowy@cs.williams.edu>
2018-06-13 15:41:44 -0400

    Add more tests.

--- test/primitive.spec.ts
+++ test/primitive.spec.ts
@@ -1,14 +1,34 @@
-import '../lib/main';
-import { expect } from 'chai';
+import * as pants from '../lib/main';
+import { assert,expect } from 'chai';
 import 'mocha';
-import { Failure } from '../lib/main';
 
 describe('Failure constructor', () => {
-
   it('should consume none of the input stream', () => {
     const inputstream = "helloworld";
-    const result = new Failure(inputstream);
-    expect(result.inputstream).to.equal(inputstream);
+    const output = new pants.Failure(inputstream);
+    expect(output.inputstream).to.equal(inputstream);
   });
+});
 
+describe('Result combinator', () => {
+    it('should succeed without consuming any input', () => {
+        const inputstream = "helloworld";
+        const output = pants.result(true)(inputstream);
+        expect(output.inputstream).to.equal(inputstream);
+    });
+});
+
+describe('Zero combinator', () => {
+    it('should fail and consume no input', () => {
+        const inputstream = "helloworld";
+        const output = pants.zero<string>()(inputstream);
+        expect(output.inputstream).to.equal(inputstream);
+        // TODO: this test does not work for not-obvious reasons
+        // switch(output.tag) {
+        //     case "failure": assert(true);
+        //     default:
+        //         console.log(output.tag);
+        //         assert.fail();
+        // }
+    });
 });

1985e5f38dad51ef2a5289e9c9fabbd0ddb75e11
Peter Zhao <pjz1@williams.edu>
2019-07-15 11:42:57 -0400

    added edit object

--- lib/Edit/Edit.ts
+++ lib/Edit/Edit.ts
@@ -1,5 +1,44 @@
 import { Option } from "space-lift";
-export interface Edit {
-    rootCause(): Option<Edit>;
-    explanation(): string;
+import { ErrorType } from "../Errors/ErrorType";
+
+export class Edit {
+    private _input: string;
+    private _error: ErrorType;
+
+    constructor(input: string, error: ErrorType){
+        this._input = input;
+        this._error = error;
+    }
+
+    /*
+    Performs one instance of a levenshtein calculation 
+    given two strings, and returns the distance. The 
+    complexity of this method is O(mn), where m and n 
+    are the lengths of the current and optimal string
+    */
+    private levenshtein(curr: string, opt:string): number {
+
+        return 2;
+    }
+
+    /*
+    Given a particular error, it creates a non-infinite 
+    space to search for the minimum edit distance via 
+    heuristics. Returns both the minimum edit distance
+    of a string that fixes the error, and returns that 
+    string. The complexity is O(kmn), where k is the 
+    size of the search space.
+    */
+    minEdit(error: ErrorType): [number, string]{
+        return [2,"k"];
+    }
+
+    /*
+    Returns the alternate string that satisfied the 
+    error
+    */
+    alternateString(): string{
+        return this.minEdit(this._error)[1];
+    }
+
 }
